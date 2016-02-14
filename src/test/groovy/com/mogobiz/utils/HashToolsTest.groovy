f9fb7874ffce47db120277768da285c65ab12f7f
Stéphane Manciot <stephane.manciot@ebiznext.com>
2016-02-14 09:24:24 +0100

    minor updates

--- src/test/groovy/com/mogobiz/utils/HashToolsTest.groovy
+++ src/test/groovy/com/mogobiz/utils/HashToolsTest.groovy
@@ -16,17 +16,15 @@ class HashToolsTest extends GroovyTestCase {
     final tmp = System.getProperty('java.io.tmpdir') ?: '/tmp'
 
     def encoded
-    def decoded
     File copy
 
     void setUp() {
         File logo = new File(ImageToolsTest.class.getResource("logoPDF.png").path)
         assertNotNull(logo)
         encoded = encodeBase64(logo, true)
-        decoded = decodeBase64(encoded)
         copy = new File(tmp, logo.getName())
         FileOutputStream fos = new FileOutputStream(copy)
-        fos.write(decoded)
+        fos.write(decodeBase64(encoded))
         fos.close()
     }
 
