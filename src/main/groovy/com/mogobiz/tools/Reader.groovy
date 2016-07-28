e9b27c889f564c6f9b5d0270c9846ae5fa3079d2
Stephane Manciot <stephane.manciot@ebiznext.com>
2016-07-28 09:34:53 +0300

    update Reader api

--- src/main/groovy/com/mogobiz/tools/Reader.groovy
+++ src/main/groovy/com/mogobiz/tools/Reader.groovy
@@ -7,7 +7,6 @@ package com.mogobiz.tools
 import groovy.util.logging.Log4j
 import rx.Subscriber
 import rx.Subscription
-import rx.subscriptions.Subscriptions
 
 /**
  *
@@ -17,16 +16,22 @@ final class Reader {
     private Reader(){}
 
     static final Closure<String> identity = {String l -> l}
-    static final Closure<String> trim = {String l -> l.trim()}
+    static final Closure<String> trim = {String l -> l?.trim()}
+    static final Closure<String> unDoubleQuotes = { String l -> l?.replaceAll("^\"", "")?.replaceAll("\"\$", "")}
+    static final Closure<String> trimAndUnDoubleQuotes = (trim >> unDoubleQuotes)
 
-    static Map<String, String> convertLineToMap(String[] keys, String line, String separator = ';', Closure<String> transformation = identity){
-        def tokens = transformation(line).split(separator)
+    static Map<String, String> convertLineToMap(
+            String[] keys,
+            String line,
+            String separator = ';',
+            Closure<String> transformation = identity){
+        String[] tokens = transformation(line).split(separator)
         def map = [:]
         if(tokens.size() == keys.size()){
             final len = tokens.size() - 1
             (0..len).each {i ->
-                final k = keys[i].trim().replaceAll("^\"", "").replaceAll("\"\$", "")
-                final v = tokens[i].trim().replaceAll("^\"", "").replaceAll("\"\$", "")
+                final k = keys[i]
+                final v = trimAndUnDoubleQuotes.call(tokens[i])
                 map[k] = v.length() > 0 ? v : null
             }
         }
@@ -36,14 +41,14 @@ final class Reader {
         return map
     }
 
-    static rx.Observable<Map> parseText(
+    static rx.Observable<CsvLine> parseText(
             final String text,
             String separator = ';',
             Closure<String> keysTransformation = identity,
             Closure<String> tokensTransformation = identity){
-        rx.Observable.create(new rx.Observable.OnSubscribe<Map>() {
+        rx.Observable.create(new rx.Observable.OnSubscribe<CsvLine>() {
             @Override
-            void call(Subscriber<? super Map> subscriber) {
+            void call(Subscriber<? super CsvLine> subscriber) {
                 def subscription = new InnerSubscription()
                 subscriber.add(subscription)
                 try{
@@ -51,11 +56,17 @@ final class Reader {
                     subscriber.onStart()
                     text.eachLine {String line, int count ->
                         if(count == 0){
-                            keys = keysTransformation(line).split(separator)
+                            keys = keysTransformation(line).split(separator).collect {trimAndUnDoubleQuotes(it)}
                         }
                         else if(line.trim().length() > 0){
                             if(!subscription.isUnsubscribed()) {
-                                subscriber.onNext(convertLineToMap(keys, line, separator, tokensTransformation))
+                                subscriber.onNext(
+                                        new CsvLine(
+                                                keys: keys,
+                                                fields: convertLineToMap(keys, line, separator, tokensTransformation),
+                                                number: count+1
+                                        )
+                                )
                             }
                         }
                     }
@@ -70,11 +81,21 @@ final class Reader {
         })
     }
 
-    static rx.Observable<CsvLine> parseCsvFile(final String filename, final String charset = 'UTF-8', final String separator = ';', Closure<String> transformation = identity){
-        parseCsvFile(new File(filename), charset, separator, transformation)
+    static rx.Observable<CsvLine> parseCsvFile(
+            final String filename,
+            final String charset = 'UTF-8',
+            final String separator = ';',
+            Closure<String> keysTransformation = identity,
+            Closure<String> tokensTransformation = identity){
+        parseCsvFile(new File(filename), charset, separator, keysTransformation, tokensTransformation)
     }
 
-    static rx.Observable<CsvLine> parseCsvFile(final File file, final String charset = 'UTF-8', final String separator = ';', Closure<String> transformation = identity){
+    static rx.Observable<CsvLine> parseCsvFile(
+            final File file,
+            final String charset = 'UTF-8',
+            final String separator = ';',
+            Closure<String> keysTransformation = identity,
+            Closure<String> tokensTransformation = identity){
         rx.Observable.create(new rx.Observable.OnSubscribe<CsvLine>() {
             @Override
             void call(Subscriber<? super CsvLine> subscriber) {
@@ -86,13 +107,13 @@ final class Reader {
                     file.eachLine(charset, {String line, lineNumber ->
                         if(line.trim().length() > 0){
                             if(lineNumber == 1){
-                                keys = transformation(line).split(separator).collect {it.trim().replaceAll("^\"", "").replaceAll("\"\$", "")}
+                                keys = keysTransformation(line).split(separator).collect {trimAndUnDoubleQuotes(it)}
                             }
                             else if(!subscription.isUnsubscribed()){
                                 subscriber.onNext(
                                         new CsvLine(
                                                 keys: keys,
-                                                fields: convertLineToMap(keys, line, separator, transformation),
+                                                fields: convertLineToMap(keys, line, separator, tokensTransformation),
                                                 number: lineNumber
                                         )
                                 )
